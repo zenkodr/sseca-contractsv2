450e74ffb1118fb0689950be9a86d968866453d5
Joji Martinez <73366769+zenkodr@users.noreply.github.com>
2023-12-28 10:06:18 -0800

    y

--- contracts/FundMe-v2.sol
+++ contracts/FundMe-v2.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
