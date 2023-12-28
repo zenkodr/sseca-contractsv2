e89afd0fe348f19f68adae8fc51e8fefb524d750
Joji Martinez <73366769+zenkodr@users.noreply.github.com>
2023-12-27 19:40:26 -0800

    ch

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.23;
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CrowdFunding is Ownable, ReentrancyGuard {
+contract FundMe is Ownable, ReentrancyGuard {
     struct Campaign {
         address owner;
         string title;
@@ -27,6 +27,9 @@ contract CrowdFunding is Ownable, ReentrancyGuard {
         uint256 _deadline,
         string memory _image
     ) public onlyOwner nonReentrant returns (uint256) {
+
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
         require(
             bytes(_title).length >= 3,
             "The title of atleast 3 characters is required"
@@ -35,21 +38,15 @@ contract CrowdFunding is Ownable, ReentrancyGuard {
             bytes(_description).length >= 25,
             "The description of atleast 25 characters is required"
         );
-        require(bytes(_imageURL).length > 0, "The image URL is required");
+        require(bytes(_image).length > 0, "The image URL is required");
         require(
-            _targetAmount > 0.1 ether,
+            _target > 0.1 ether,
             "The target amount must be above 0.1 ether"
         );
         require(
             _deadline > block.timestamp,
             "The deadline must be a future date"
         );
-        require(
-            numberOfCampaigns < 1000,
-            "Too many campaigns, operation costly"
-        );
-
-        Campaign storage campaign = campaigns[numberOfCampaigns];
 
         campaign.owner = _owner;
         campaign.title = _title;
@@ -61,24 +58,13 @@ contract CrowdFunding is Ownable, ReentrancyGuard {
 
         numberOfCampaigns++;
 
-        emit Action(
-            campaign.id,
-            "Campaign Created",
-            msg.sender,
-            block.timestamp
-        );
-
         return numberOfCampaigns - 1;
     }
 
     function donateToCampaign(uint256 _id) public payable nonReentrant {
-        require(msg.value > 0, "The amount must be above 0");
-        require(
-            campaigns[_id].owner != msg.sender,
-            "The campaign owner cannot back their own campaign"
-        );
-
         uint256 amount = msg.value;
+        
+        require(msg.value > 0, "The amount must be above 0");
 
         Campaign storage campaign = campaigns[_id];
 
@@ -92,13 +78,6 @@ contract CrowdFunding is Ownable, ReentrancyGuard {
         if (sent) {
             campaign.amountCollected = campaign.amountCollected + amount;
         }
-
-        emit Action(
-            campaign._id,
-            "Campaign Backed",
-            msg.sender,
-            block.timestamp
-        );
     }
 
     function getDonators(
